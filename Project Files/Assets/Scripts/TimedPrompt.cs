33a4ad5c41ff4084164ffc2c6413f181752332dc
ten18 <48300794+ten18@users.noreply.github.com>
2019-09-15 16:07:49 -0400

    Add files via upload

---
+++ Project Files/Assets/Scripts/TimedPrompt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedPrompt : MonoBehaviour
+{
+    bool promptPrinted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        promptPrinted = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > 3 && promptPrinted)
+        {
+            print("It's been three seconds!");
+            promptPrinted = false;
+        }
+    }
+}
