4a8a1d79f1114a79144a82bf08a81ce7151ac8ef
ten18 <48300794+ten18@users.noreply.github.com>
2019-09-23 11:49:19 -0400

    Add files via upload

--- Project Files/Assets/Scripts/Mining.cs
+++ Project Files/Assets/Scripts/Mining.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Mining : MonoBehaviour
 {
-    int bronze, bronzeSupply;
-    int silver, silverSupply;
+    int bronze, bronzeSupply, bronzeShift;
+    int silver, silverSupply, silverShift;
     float mineNow;
     float miningSpeed;
+    public Material bronzeColor;
+    //public GameObject cubePrefab;
     void Start()
     {
         bronze = 0;
@@ -16,6 +18,8 @@ public class Mining : MonoBehaviour
         mineNow = miningSpeed;
         bronzeSupply = 3;
         silverSupply = 2;
+        bronzeShift = 0;
+        silverShift = 0;
     }
 
     // Update is called once per frame
@@ -29,11 +33,22 @@ public class Mining : MonoBehaviour
             {
                 bronzeSupply--;
                 bronze++;
+                //cubePosition = new Vector3(0,0,0);
+                //Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+                GameObject bronzeOre = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                bronzeOre.GetComponent<Renderer>().material = bronzeColor;
+                bronzeOre.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                bronzeOre.transform.position = new Vector3(-7f+bronzeShift, 0, 0);
+                bronzeShift = bronzeShift + 2;
             }
             else if (silverSupply > 0)
             {
                 silverSupply--;
                 silver++;
+                GameObject silverOre = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                silverOre.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                silverOre.transform.position = new Vector3(3f+silverShift, 0, 0);
+                silverShift = silverShift + 2;
             }
 
             print("Bronze: " + bronze + " ... Silver: " + silver);
