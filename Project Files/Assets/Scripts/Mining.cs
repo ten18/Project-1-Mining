f010d08b3000dd75fb2a811c975555139b35fb6b
ten18 <48300794+ten18@users.noreply.github.com>
2019-10-02 11:05:38 -0400

    Add files via upload

--- Project Files/Assets/Scripts/Mining.cs
+++ Project Files/Assets/Scripts/Mining.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Mining : MonoBehaviour
 {
-    int bronze, bronzeSupply, bronzeShift;
-    int silver, silverSupply, silverShift;
+    int bronze, bronzeShift;
+    int silver, silverShift;
+    int gold, goldShift;
     float mineNow;
-    float miningSpeed;
+    public float miningSpeed;
     public Material bronzeColor;
-    //public GameObject cubePrefab;
+    public Material goldColor;
     void Start()
     {
         bronze = 0;
         silver = 0;
+        gold = 0;
         miningSpeed = 3;
         mineNow = miningSpeed;
-        bronzeSupply = 3;
-        silverSupply = 2;
         bronzeShift = 0;
         silverShift = 0;
+        goldShift = 0;
     }
 
     // Update is called once per frame
@@ -29,29 +30,37 @@ public class Mining : MonoBehaviour
         {
             mineNow += miningSpeed;
 
-            if (bronzeSupply > 0)
+            if (bronze < 4) //nothing but bronze spawns if there is not enough of it
             {
-                bronzeSupply--;
                 bronze++;
-                //cubePosition = new Vector3(0,0,0);
-                //Instantiate(cubePrefab, cubePosition, Quaternion.identity);
                 GameObject bronzeOre = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 bronzeOre.GetComponent<Renderer>().material = bronzeColor;
                 bronzeOre.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
                 bronzeOre.transform.position = new Vector3(-7f+bronzeShift, 0, 0);
-                bronzeShift = bronzeShift + 2;
+                bronzeShift = bronzeShift + 2; //spawn next bronze to the right of the previous
             }
-            else if (silverSupply > 0)
+            else //silver spawns when there is enough bronze
             {
-                silverSupply--;
                 silver++;
                 GameObject silverOre = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 silverOre.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
                 silverOre.transform.position = new Vector3(3f+silverShift, 0, 0);
-                silverShift = silverShift + 2;
+                silverShift = silverShift + 2; 
             }
 
-            print("Bronze: " + bronze + " ... Silver: " + silver);
+            if (bronze == 2 && silver == 2) //spawn a gold ONLY if this is true
+            {
+                gold++;
+                GameObject goldOre = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                goldOre.GetComponent<Renderer>().material = goldColor;
+                goldOre.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                goldOre.transform.position = new Vector3(1f+goldShift, 3f, 0);
+                goldShift = goldShift + 2;
+            }
+
+
+
+            print("Bronze: " + bronze + "  Silver: " + silver + "  Gold: " + gold);
         }
     }
 }
